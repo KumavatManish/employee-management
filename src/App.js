988a7aa3182f5a5692887aa68f508948e2469b42
Manish <kumavatmanish2@gmail.com>
2023-02-23 15:25:55 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import CRUD from './CRUD.js';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{paddingBottom:"20px"}}>Employee Management System</h1>
+      <CRUD/>
+    </div>
+  );
+}
+
+export default App;
